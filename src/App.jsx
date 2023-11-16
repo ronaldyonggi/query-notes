380ad57e52e053a9b4e73f937f777187d62ac130
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-16 17:11:41 +0700

    add react query notes mutation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import './App.css'
-import { getNotes } from '../requests'
+import { getNotes, createNote, updateNote } from './requests'
 
 function App() {
+  const queryClient = useQueryClient()
+
+  // ========================== ADDING NEW NOTE =====================
+  const newNoteMutation = useMutation({ 
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes']})
+    }
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    newNoteMutation.mutate({ content, important: true })
   }
 
+  // ========================== TOGGLE NOTE IMPORTANCE =====================
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes']})
+    }
+  })
+
   const toggleImportance = note => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation({...note, important: !note.important})
   }
 
   const result = useQuery({
     queryKey: ['notes'],
     queryFn: getNotes
   })
-  console.log(JSON.parse(JSON.stringify(result)))
+  // console.log(JSON.parse(JSON.stringify(result)))
 
   if ( result.isLoading ) {
     return <div>loading data...</div>
