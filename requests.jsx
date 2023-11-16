ea1fb991bc2766e2d8e9784e64e5f2f2f752807e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-16 15:37:38 +0700

    add createNote service

--- requests.jsx
+++ requests.jsx
@@ -4,3 +4,6 @@ const baseUrl = 'http://localhost:3001/notes'
 
 export const getNotes = () => 
     axios.get(baseUrl).then(res => res.data)
+
+export const createNote = newNote =>
+    axios.post(baseUrl, newNote).then(res => res.data)
