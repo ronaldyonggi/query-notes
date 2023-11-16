41ca1019e47419747a418446a3dcab0e465fd80d
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-16 14:50:08 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    console.log(content)
+  }
+
+  const toggleImportance = note => {
+    console.log('toggle importance of', note.id)
+  }
+
+  const notes = []
+
+  return (
+    <div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
+
+      {notes.map(note => 
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong>{ note.important ? 'important' : 'not important'}</strong>
+        </li>
+        )}
+    </div>
+  )
+}
+
+export default App
