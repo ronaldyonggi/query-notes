dd16baaf4ae02341869b3911237f1d677e7c28ca
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-21 16:13:48 +0800

    displaying Note moved to a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import './App.css'
-import { getNotes, updateNote } from './requests'
+import { getNotes } from './requests'
 import NoteForm from './components/NoteForm'
+import Note from './components/Note'
 
 function App() {
-  const queryClient = useQueryClient()
-
-  // ========================== TOGGLE NOTE IMPORTANCE =====================
-
-  const updateNoteMutation = useMutation({
-    mutationFn: updateNote,
-    onSuccess: (changedNote) => {
-      const notes = queryClient.getQueryData(['notes'])
-      const mutatedNotes = notes.map(n => n.id === changedNote.id ? changedNote : n)
-      queryClient.setQueryData(['notes'], mutatedNotes)
-      // queryClient.invalidateQueries({ queryKey: ['notes']})
-    }
-  })
-
-  const toggleImportance = note => {
-    updateNoteMutation.mutate({...note, important: !note.important})
-  }
-
-
   // ========================== DISPLAY NOTES =====================
 
   const result = useQuery({
@@ -48,11 +30,7 @@ function App() {
       <NoteForm />
 
       {notes.map(note => 
-        <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} | 
-          <strong>{ note.important ? 'important' : 'not important'}</strong>
-          |
-        </li>
+        <Note key={note.id} note={note}/>
         )}
     </div>
   )
